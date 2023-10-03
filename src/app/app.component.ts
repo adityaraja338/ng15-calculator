01720951f2b03e1d1d5cd5ca8acf007838a8ffe4
Aditya Raj Aryan <aditya.a@codezen.in>
2023-10-03 12:45:56 +0530

    length added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,54 @@ export class AppComponent {
 
   darkMode=true;
   color='orange';
-  calculatorMode = 'Standard';
+  calculatorMode = 'Length';
   hamburgerOpen = false;
 
-  constructor(private calculatorService :CalculatorService) {
+  length1:string="";
+  length2:string="";
+  lengthMode1=0;
+  lengthMode2 =1;
+
+  orange='#FD7014';
+  green='#14FD63'
+  white='#EEE';
+  dark='#393E46';
+
+  activeColor=this.orange;
+
+  constructor(protected calculatorService :CalculatorService) {
+  }
+
+  addToExpression(value: string) {
+    this.calculatorService.addToExpression(value);
+  }
+
+  clearCurrent() {
+    this.calculatorService.clearCurrent();
+  }
+
+  clearAll() {
+    this.calculatorService.clearAll();
+  }
+
+  deleteLastCharacter() {
+    this.calculatorService.deleteLastCharacter();
+  }
+
+  evaluateExpression() {
+    this.calculatorService.evaluateExpression();
+  }
+
+  result:any;
+  currentExp:any;
+
+  mOptions = [1,100,3.280839895];
+  cmOptions = [0.01, 1, 0.03280839895];
+  feetOptions = [1/3.280839895, 1/0.03280839895,1];
+  convert(){
+    this.lengthMode1 == 0 ? this.length2 = +this.length1*this.mOptions[this.lengthMode2]+'':null;
+    this.lengthMode1 == 1 ? this.length2 = +this.length1*this.cmOptions[this.lengthMode2]+'':null;
+    this.lengthMode1 == 2 ? this.length2 = +this.length1*this.feetOptions[this.lengthMode2]+'':null;
   }
 
   protected readonly console = console;
--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -1,81 +1,49 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculatorService {
-  private currentInput: string = ''; // Stores the current input expression
-  private result: number | null = null; // Stores the calculated result
+  private currentExpression: string = '';
+  private lastResult: number | null = null;
+  private lastExpression: number | null = null;
 
   constructor() {}
 
-  // Method to handle digit input
-  inputDigit(digit: string): void {
-    this.currentInput += digit;
+  addToExpression(value: string): void {
+    this.currentExpression += value;
   }
 
-  // Method to handle operator input
-  inputOperator(operator: string): void {
-    if (this.currentInput !== '') {
-      this.currentInput += ' ' + operator + ' ';
-    }
-  }
-
-  // Method to handle negation
-  negate(): void {
-    if (this.currentInput === '' || this.currentInput.endsWith(' ')) {
-      this.currentInput += '-';
-    }
-  }
-
-  // Method to handle small brackets
-  inputSmallBracket(): void {
-    this.currentInput += '(';
-  }
-
-  // Method to handle closing small brackets
-  inputClosingSmallBracket(): void {
-    this.currentInput += ')';
-  }
-
-  // Method to handle percentage
-  inputPercentage(): void {
-    this.currentInput += '%';
-  }
-
-  // Method to clear the current input statement
   clearCurrent(): void {
-    this.currentInput = '';
+    this.currentExpression = '';
   }
 
-  // Method to clear the whole calculation
   clearAll(): void {
-    this.currentInput = '';
-    this.result = null;
+    this.currentExpression = '';
+    this.lastResult = null;
+    this.lastExpression = null;
   }
 
-  // Method to delete the last character
   deleteLastCharacter(): void {
-    this.currentInput = this.currentInput.slice(0, -1);
+    this.currentExpression = this.currentExpression.slice(0, -1);
   }
 
-  // Method to evaluate the current expression
-  calculate(): void {
+  evaluateExpression(): void {
     try {
-      // Using eval for simplicity, consider a safer parser for production
-      this.result = eval(this.currentInput);
-    } catch (e) {
-      this.result = null; // Handle invalid expressions gracefully
+      this.lastResult = eval(this.currentExpression);
+      this.lastExpression = this.lastResult;
+      this.currentExpression = this.lastResult+"";
+    } catch (error) {
+      this.lastResult = null;
     }
   }
 
-  // Method to get the current input
-  getCurrentInput(): string {
-    return this.currentInput;
+  getCurrentExpression() {
+    return {last: this.lastExpression, current: this.currentExpression};
   }
 
-  // Method to get the calculated result
-  getResult(): number | null {
-    return this.result;
+  getLastResult(): number | null {
+    return this.lastResult;
   }
+
 }
